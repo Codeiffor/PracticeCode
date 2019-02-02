8103b0e12fd62953a661b3706207e6341db734f3
codeiffor <sid96@protonmail.com>
2019-02-03 04:51:25 +0530

    HTMLScraper

---
+++ download_page/downloadPage.js
@@ -0,0 +1,33 @@
+const http = require("http")
+const path = require("path")
+const fs = require("fs")
+const uuidv1 = require("uuid/v1")
+
+const downloadPage = (url = 'http://nodeprogram.com') => {
+    const folderName = uuidv1()
+    fs.mkdirSync(folderName)
+    const fetchPage = (url ,callback) => {
+        http.get(url, (response) => {
+            let buff = ''
+            response.on('data', (chunk) => {
+                buff += chunk
+            })
+            response.on('end', () => {
+                callback(null, buff)
+            })
+        }).on('error', (error) => {
+            console.error(`got error : ${error.message}`)
+            callback(error)
+        })
+    }
+    console.log('downloading url ',url)
+    fetchPage(url, (error, data) => {
+        if(error)
+            console.log(error)
+        fs.writeFileSync(path.join(__dirname, folderName, 'url.txt'), url+'\nfire')
+        fs.writeFileSync(path.join(__dirname, folderName, 'index.html'), data+'\n')
+        console.log('downloading complete in folder ', folderName)
+    })
+}
+
+downloadPage(process.argv[2])
