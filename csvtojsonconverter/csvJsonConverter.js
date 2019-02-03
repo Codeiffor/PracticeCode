5e29b2f240f12336ef4f376954e2de4be236028a
codeiffor <sid96@protonmail.com>
2019-02-03 19:29:18 +0530

    error handler for csvtojson

--- csvtojsonconverter/csvJsonConverter.js
+++ csvtojsonconverter/csvJsonConverter.js
@@ -4,9 +4,14 @@ const fs = require('fs')
 
 const convertCsvToJson = (filename = 'customer-data.csv') => {
     let csvFilePath = path.join(__dirname,filename)
-    csvtojson().fromFile(csvFilePath)
+    csvtojson()
+    .fromFile(csvFilePath)
+    .on('error', (error) => {
+        console.log('error found : \n',error)
+    })
     .then((jsonObject) => {
         fs.writeFileSync(path.join(__dirname,filename.replace('.csv','.json')),JSON.stringify(jsonObject))
+        console.log(`csv converted to json and saved on file "${filename.replace('.csv','.json')}"`)
     })
 }
 
